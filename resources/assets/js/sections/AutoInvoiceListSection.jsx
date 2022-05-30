5ab255343f2c9a3485ca486c0b0a288a55a7df33
Christian <cris@nms.ph>
2022-05-30 16:15:20 +0800

    #2 invoices crud added actions column in datagrid

--- resources/assets/js/sections/AutoInvoiceListSection.jsx
+++ resources/assets/js/sections/AutoInvoiceListSection.jsx
@@ -20,7 +20,54 @@ import Avatar from "@mui/material/Avatar";
 import Chip from "@mui/material/Chip";
 import MyMoment from "../components/MyMoment";
 import { DataGrid } from "@mui/x-data-grid";
-import { useDemoData } from "@mui/x-data-grid-generator";
+
+const columns = [
+  {
+    field: 'action',
+    headerName: 'Actions',
+    sortable: false,
+    renderCell: (params) => {
+      const onClick = (e) => {
+        e.stopPropagation(); // don't select this row after clicking
+
+        const row = params.row;
+        return alert(JSON.stringify(row, null, 4));
+      };
+
+      return <Button onClick={onClick}>Click</Button>;
+    },
+  },
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''
+      }`,
+  }];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 const SearchForm = (props) => {
   // const myLeavesData = React.useContext(leavesDataContext);
@@ -130,12 +177,6 @@ export default function AutoInvoiceListSection(){
   const [leavesData,setLeavesData] = useState(null);
   const [pageSize, setPageSize] = useState(5);
 
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 100,
-    maxColumns: 6,
-  });
-
   function leavesDataResultsCallbackParent(response){
     setLeavesData(response);
   }
@@ -150,6 +191,9 @@ export default function AutoInvoiceListSection(){
       rowsPerPageOptions={[5, 10, 20]}
       pagination
       autoHeight
-      {...data} />
+      {...{
+        columns,
+        rows
+      }} />
   </React.Fragment>
 }
