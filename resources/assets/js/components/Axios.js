0863a3fe6606e239542d23749073dd5730bc3919
Christian <darkcolonist@gmail.com>
2022-06-03 12:18:55 +0800

    #1 environment setup reload page if error 419 (token mismatch usually)

--- resources/assets/js/components/Axios.js
+++ resources/assets/js/components/Axios.js
@@ -2,28 +2,32 @@ import axios from 'axios';
 axios.defaults.headers.common['X-CSRF-TOKEN'] = appToken;
 const instance = axios.create({ baseURL: appBaseURL });
 
+const fallbackAndReload = (message) => {
+  console.log(message+" detected, reloading page in 2 seconds.");
+
+  setTimeout(() => {window.location.reload()}, 2000);
+}
+
 /**
  * covers:
  *  - token_mismatch
  */
 instance.interceptors.response.use(
   response => {
-    // console.log("found", response.data);
     if (response.data.type !== undefined 
         && response.data.type === "token_mismatch"){
-      console.log("token mismatch detected, reloading page.");
-      window.location.reload();
-      // return Promise.reject("token_mismatch");
+      fallbackAndReload("token mismatch");
     }
 
     return response;
   }
-  // ,
-  // error => {    
-  //   if (error.response && 419 === error.response.status) {
-  //   }
+  ,
+  error => {    
+    if (error.response && 419 === error.response.status) {
+      fallbackAndReload(error.statusText);
+    }
 
-  // }
+  }
 )
 
 export default instance
